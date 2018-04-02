933cf3bbfac7a54696716d741b344e1b56503c9d
mazhen <mazhenhit@foxmail.com>
2018-04-02 18:48:16 +0800

    by mazhen

---
+++ 055.java
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: if string A contains all of the characters in B return true else return false
+     */
+    bool compareStrings(string &A, string &B) {
+        // write your code here
+    }
+};
