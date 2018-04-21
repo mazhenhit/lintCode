7a770b1b5c5ba2b9df247d59800c01fb8ef2f2eb
MichaelHit <mazhenhit@users.noreply.github.com>
2018-04-21 16:21:39 +0800

    Create 096.java

---
+++ 096.java
@@ -0,0 +1,24 @@
+if (head == null)
+			return null;
+
+		ListNode leftDummy = new ListNode(0);
+		ListNode rightDummy = new ListNode(0);
+
+		ListNode left = leftDummy;
+		ListNode right = rightDummy;
+
+		while (head != null) {
+			if (head.val < x) {
+				left.next = head;
+				left = head;
+			} else {
+				right.next = head;
+				right = head;
+			}
+			head = head.next;
+		}
+
+		right.next = null;
+		left.next = rightDummy.next;
+
+		return leftDummy.next;
