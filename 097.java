96575d5493914445fa09d11035c410c6784cfc7a
MichaelHit <mazhenhit@users.noreply.github.com>
2018-05-06 07:37:05 +0800

    Create 097.java

---
+++ 097.java
@@ -0,0 +1,27 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+    public int maxDepth(TreeNode root) {
+        if(root == null)
+            return 0;
+            
+        int lenth1 = maxDepth(root.left);
+        int lenth2 = maxDepth(root.right);
+        
+        return lenth1 > lenth2 ? lenth1 + 1 : lenth2 + 1; 
+    }
+}
